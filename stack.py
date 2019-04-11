cb4aaa81c684fda7658d74428ecdbe8a81e69d90
zhenxinqin <43924717+zhenxinqin@users.noreply.github.com>
2019-04-11 20:18:48 +0800

    Create stack.py

---
+++ stack.py
@@ -0,0 +1,49 @@
+#coding:utf-8
+
+class Stack(object):
+	'''栈'''
+	def __init__(self):
+		'''初始化一个栈'''
+		self.__list = []
+
+	def push(self,item):
+		'''入栈'''
+		self.__list.append(item)
+
+	def pop(self):
+		'''弹出'''
+		return self.__list.pop()
+
+	def peek(self):
+		'''返回栈顶元素'''
+		if self.__list:
+			return self.__list[-1]
+		else:
+			return None
+
+	def is_emperty(self):
+		'''判断是否为空'''
+		return self.__list == []
+
+	def size(self):
+		'''栈的大小'''
+		return len(self.__list)
+
+if __name__ == '__main__':
+	stack1 = Stack()
+	stack1.push(1)
+	stack1.push(2)
+	stack1.push(3)
+	stack1.push(4)
+	stack1.push(5)
+	print stack1.peek()
+	print stack1.pop()
+	print stack1.pop()
+	print stack1.pop()
+	print stack1.pop()
+	print stack1.pop()
+	print stack1.peek()
+
+
+
+
